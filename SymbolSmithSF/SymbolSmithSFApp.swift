1c99794c121ed54b5fe756d257bc26aec3ed9800
Ned Hogan <nedhogan@me.com>
2026-02-13 08:31:40 -0800

    Initial Commit

---
+++ SymbolSmithSF/SymbolSmithSFApp.swift
@@ -0,0 +1,17 @@
+//
+//  SymbolSmithSFApp.swift
+//  SymbolSmithSF
+//
+//  Created by Edward Hogan on 2/13/26.
+//
+
+import SwiftUI
+
+@main
+struct SymbolSmithSFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SymbolSmithSFTests/SymbolSmithSFTests.swift
@@ -0,0 +1,16 @@
+//
+//  SymbolSmithSFTests.swift
+//  SymbolSmithSFTests
+//
+//  Created by Edward Hogan on 2/13/26.
+//
+
+import Testing
+
+struct SymbolSmithSFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
